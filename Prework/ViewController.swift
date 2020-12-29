0ba7c278103d725310864c4d53f38bb35439f6cb
chhodensherpa <76025934+chhodensherpa@users.noreply.github.com>
2020-12-29 15:36:35 -0700

    Worked on the Tip Calculator App

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+                billAmountTextField.keyboardType = .asciiCapableNumberPad
+
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the bill
+               let bill = Double(billAmountTextField.text!) ?? 0
+           
+           
+               // Calculate the tip and total
+               let tipPercentages = [0.15, 0.18, 0.2]
+               let tip = bill*tipPercentages[tipControl.selectedSegmentIndex]
+               let total = bill+tip
+               
+               
+               // Update the tip and total
+               tipLabel.text = String(format:"$%.2f",tip)
+               totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onClickResetButton(_ sender: Any) {
+        billAmountTextField.text = ""
+                tipLabel.text = "$0.00"
+                totalLabel.text = "$0.00"
 
+    }
+    
 }
 
