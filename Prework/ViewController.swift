a3c9c6d364f33cf639fca5fa2a336db56c6cf803
chhodensherpa <76025934+chhodensherpa@users.noreply.github.com>
2020-12-29 16:30:27 -0700

    Progress on the App

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -10,31 +10,36 @@ import UIKit
 class ViewController: UIViewController {
 
     
+   
     @IBOutlet weak var billAmountTextField: UITextField!
     
-    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var totalLabel: UILabel!
     
-    @IBOutlet weak var tipLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "Tip Calculator"
-                billAmountTextField.keyboardType = .asciiCapableNumberPad
+    
 
     }
 
+   
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
+
     }
+    
     
     
     @IBAction func calculateTip(_ sender: Any) {
-        // Get the bill
-               let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
            
            
                // Calculate the tip and total
@@ -44,15 +49,16 @@ class ViewController: UIViewController {
                
                
                // Update the tip and total
-               tipLabel.text = String(format:"$%.2f",tip)
+               tipPercentageLabel.text = String(format:"$%.2f",tip)
                totalLabel.text = String(format: "$%.2f", total)
+
     }
+    
     
     @IBAction func onClickResetButton(_ sender: Any) {
         billAmountTextField.text = ""
-                tipLabel.text = "$0.00"
-                totalLabel.text = "$0.00"
-
+              tipPercentageLabel.text = "$0.00"
+              totalLabel.text = "$0.00"
     }
     
 }
